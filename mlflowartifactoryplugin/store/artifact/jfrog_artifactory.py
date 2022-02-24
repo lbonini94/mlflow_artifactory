ab417e5045817542d97ad333231720d0cd74c50a
Lucas A. Bonini <bonini.gfbpa@gmail.com>
2022-02-24 18:16:59 -0300

    test 1

---
+++ mlflowartifactoryplugin/store/artifact/jfrog_artifactory.py
@@ -0,0 +1,107 @@
+
+from mlflow.store.artifact.artifact_repo import ArtifactRepository
+from artifactory import ArtifactoryPath
+from mlflow.exceptions import MlflowException
+import os
+import urllib
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class JfrogArtifactoryRepository(ArtifactRepository):
+    """Stores Artifacts on JFrog Artifactory
+    """
+    
+    is_plugin=True
+    
+    def __init__(self,
+                 artifact_uri: str, # "artifactory://mlflow",
+                 ):
+        
+        super(JfrogArtifactoryRepository, self).__init__(artifact_uri)
+        
+        logger.info('JFROG STARTED')
+        # "http://my-jfrog-server/artifactory/mlflow"
+        self.jfrog_endpoint_url = os.environ.get("JFROG_ENDPOINT_URL")
+        assert self.jfrog_endpoint_url, 'Please set JFROG_ENDPOINT_URL'
+        
+        self.auth: dict = self._auth()
+        self.jfrog_artifactory_uri = artifact_uri
+        
+    def _auth(self):
+
+        if "JFROG_ARTIFACTORY_TOKEN" in os.environ:
+            jfrog_artifactory_token = os.environ.get("JFROG_ARTIFACTORY_TOKEN")
+            path = ArtifactoryPath(
+                self.jfrog_endpoint_url,
+                token=jfrog_artifactory_token
+            )
+            if path.exists():
+                return {"token": jfrog_artifactory_token}
+                
+        elif "JFROG_ARTIFACTORY_APIKEY" in os.environ:
+            jfrog_apikey = os.environ.get("JFROG_ARTIFACTORY_APIKEY")
+            path = ArtifactoryPath(
+                self.jfrog_endpoint_url,
+                apikey=jfrog_apikey
+            )
+            if path.exists():
+                return {"apikey": jfrog_apikey}
+ 
+        else:
+            try:
+                jfrog_artifactory_username = os.environ["JFROG_ARTIFACTORY_USERNAME"]
+                jfrog_artifactory_password = os.environ["JFROG_ARTIFACTORY_PASSWORD"]
+            except KeyError:
+                raise Exception("YOU NEED TO PROVIDE SOME AUTH METHOD TO JFROG\n-APIKEY\n-TOKEN\n-USER/PASS")
+            path = ArtifactoryPath(
+                self.jfrog_endpoint_url,
+                auth=(jfrog_artifactory_username,
+        # "JFROG_ARTIFACTORY_PASSWORD" can be "JFROG_ARTIFACTORY_APIKEY"
+                    jfrog_artifactory_password), 
+            )
+            if path.exists():
+                return {"auth": (jfrog_artifactory_username,
+                                    jfrog_artifactory_password)}
+
+                
+    @staticmethod
+    def parse_artifactory_uri(uri:str):
+        """Parse an OSS URI, returning (bucket, path)"""
+        parsed = urllib.parse.urlparse(uri)
+        if parsed.scheme != "artifactory":
+            raise Exception("Not an artifactory URI: %s" % uri)
+        path = parsed.path
+        if path.startswith('/'):
+            path = path[1:]
+        return parsed.netloc, path
+    
+    def log_artifact(self, local_file, artifact_path=None):
+        path = ArtifactoryPath(
+            self.jfrog_endpoint_url + artifact_path,
+            **self.auth
+        )
+        
+        if not path.exists():
+            path.mkdir()
+        
+        path.deploy_file(local_file)
+
+    
+    def log_artifacts(self, local_dir, artifact_path=None):
+        pass
+    
+    def list_artifacts(self, path=None):
+        if not path:
+            path = ArtifactoryPath(
+                self.jfrog_endpoint_url,
+                **self.auth
+            )
+        return [p for p in path]
+    
+    def _download_file(self, remote_file_path, local_path):
+        pass
+    
+    def delete_artifacts(self, artifact_path=None):
+        raise MlflowException('Not implemented yet')
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mflow-artifactory-plugin",
-    version='0.0.1',
+    version='0.0.3',
     description='Plugin that provides Artifact Store functionality between MLFlow and JFrog',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,14 +15,14 @@ setup(
     url="https://github.com/lbonini94/mflow-artifactory-plugin",
     # Require MLflow as a dependency of the plugin, so that plugin users can simply install
     # the plugin and then immediately use it with MLflow
-    install_requires=["mlflow"],
+    install_requires=[
+        "mlflow",
+        "dohq-artifactory",
+    ],
     packages=find_packages(),
     entry_points={
         # Define a ArtifactRepository plugin for artifact URIs with scheme 'file-plugin'
         "mlflow.artifact_repository":
-            #TODO:
-            "file-plugin=mlflow_test_plugin.local_artifact:PluginLocalArtifactRepository",
-
+            "artifactory=mlflowartifactoryplugin.store.artifact.jfrog_artifactory:JfrogArtifactoryRepository",
     },
-    license="Apache License 2.0",
 )
